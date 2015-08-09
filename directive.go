152f32f16b5dc0f7dfe831e5339f6052cf559557
Viet Pham <phamhongviet52@gmail.com>
2015-08-09 19:04:49 +0700

    parse directive from config file

--- directive.go
+++ directive.go
@@ -1,53 +1,35 @@
 package main
 
 import (
-	"errors"
-	"strings"
+	"io/ioutil"
+	"encoding/json"
 )
 
-type Directive struct {
-	template string
-	result   string
-	command  string
-	tags     []string
+type Template struct {
+	Src string
+	Dest string
+	Cmd string
 }
 
-func ParseDirectives(args []string) ([]Directive, error) {
-	args_len := len(args)
-	directives := make([]Directive, args_len)
-	// for each args, parse into directives
-	for i := 0; i < args_len; i = i + 1 {
-		// split it into parts
-		// 1st part: path to template file
-		// 2nd part: path to result file
-		// 3nd part: command to execute, optional
-		// remaining parts: filter tags
-		parts := strings.Split(args[i], ":")
-		parts_len := len(parts)
-		// check number
-		if parts_len < 2 {
-			return nil, errors.New("Syntax error")
-		}
-		for i := 0; i < parts_len; i = i + 1 {
-			if len(parts[i]) == 0 {
-				return nil, errors.New("Syntax error")
-			}
-		}
-		// register directive
-		directives[i] = Directive{
-			template: parts[0],
-			result:   parts[1],
-		}
-		if parts_len > 2 {
-			directives[i].command = parts[2]
-		}
-		if parts_len > 3 {
-			directives[i].tags = make([]string, parts_len-3)
-			for j := 0; j < parts_len-3; j = j + 1 {
-				directives[i].tags[j] = parts[3+j]
-			}
-		}
-	}
+type Directive struct {
+	Name string
+	Role string
+	Status string
+	Tags []string
+	Rpc-addr string
+	Rpc-auth string
+	Templates []Template
+}
 
-	return directives, nil
+func ParseDirectives(config-file string) (Directive, error) {
+	config-json, err := ioutil.ReadFile(config-file)
+	if err != nil {
+		panic(err)
+	}
+	var directive Directive
+	err = json.Unmarshal(config-json, &directive)
+	if err != nil {
+		panic(err)
+	}
+	return directive, nil
 }
