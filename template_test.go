8cd9f43e47cd302009cd6c91d92af816dda33009
Viet Pham <phamhongviet52@gmail.com>
2015-08-09 21:35:13 +0700

    reformat

--- template_test.go
+++ template_test.go
@@ -1,43 +1,42 @@
 package main
 
 import (
-"io/ioutil"
-"fmt"
+	"fmt"
+	"io/ioutil"
 )
 
 func ExampleRenderTemplate() {
 	members := []Member{
 		{
-			Name: "web-1",
-			Addr: "172.16.0.21",
-			Port: 7946,
+			Name:   "web-1",
+			Addr:   "172.16.0.21",
+			Port:   7946,
 			Status: "alive",
 			Tags: map[string]string{
-				"app": "nginx",
+				"app":     "nginx",
 				"webport": "8080",
 			},
 		},
 		{
-			Name: "web-2",
-			Addr: "172.16.0.22",
-			Port: 7946,
+			Name:   "web-2",
+			Addr:   "172.16.0.22",
+			Port:   7946,
 			Status: "alive",
 			Tags: map[string]string{
-				"app": "nginx",
+				"app":     "nginx",
 				"webport": "8080",
 			},
 		},
 		{
-			Name: "web-3",
-			Addr: "172.16.0.23",
-			Port: 7946,
+			Name:   "web-3",
+			Addr:   "172.16.0.23",
+			Port:   7946,
 			Status: "alive",
 			Tags: map[string]string{
-				"app": "httpd",
+				"app":     "httpd",
 				"webport": "80",
 			},
 		},
-
 	}
 	RenderTemplate("test/template_1.tpl", "test/result_1.txt", members)
 	out, err := ioutil.ReadFile("test/result_1.txt")
@@ -45,10 +44,11 @@ func ExampleRenderTemplate() {
 		panic(err)
 	}
 	fmt.Println(string(out))
-/* Output:
+/*
+Output:
 BEGIN
 server web-1 at 172.16.0.21 with serf at 7946 and nginx at 8080
-server web-2 at 172.16.0.22 with serf at 7946 and nginx at 8080 
+server web-2 at 172.16.0.22 with serf at 7946 and nginx at 8080
 server web-3 at 172.16.0.23 with serf at 7946 and httpd at 80
 
 END
